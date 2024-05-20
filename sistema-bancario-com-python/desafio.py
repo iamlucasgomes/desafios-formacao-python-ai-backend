a31068e85b2fdece84d3b88fde364a0517b80559
iamlucasgomes <lucas.devjs@gmail.com>
2024-05-19 22:19:10 -0300

    chore: Refactor SistemaBancario class constructor and methods

--- sistema-bancario-com-python/desafio.py
+++ sistema-bancario-com-python/desafio.py
@@ -4,18 +4,18 @@ from uuid import uuid4
 
 class SistemaBancario:
     def __init__(self, nome: str):
-        self.nome = nome
-        self.saldo = 0
-        self.historico = []
-        self.saques = 0
-        self.ultima_data_saque = None
+        self.__nome = nome
+        self.__saldo = 0
+        self.__historico = []
+        self.__saques = 0
+        self.__ultima_data_saque = None
 
-    def deposito(self, valor: int):
+    def __deposito(self, valor: int):
         if valor <= 0:
             return print("Valor deve ser maior que zero")
         else:
-            self.saldo += valor
-            self.historico.append(
+            self.__saldo += valor
+            self.__historico.append(
                 {
                     "id": uuid4(),
                     "data": datetime.now().date().isoformat(),
@@ -24,29 +24,29 @@ class SistemaBancario:
                 }
             )
 
-            return f"Depósito realizado com sucesso! Saldo atual: {self.saldo}"
+            return print(f"Depósito realizado com sucesso! Saldo atual: {self.__saldo}")
 
-    def saque(self, valor: int):
+    def __saque(self, valor: int):
 
         hoje = datetime.now().date()
 
-        if self.saldo <= 0 or self.saldo < valor:
+        if self.__saldo <= 0 or self.__saldo < valor:
             return print("Saldo insuficiente")
 
         if valor > 500:
             return print("Valor máximo para saque é de R$500")
 
-        if self.saques >= 3:
+        if self.__saques >= 3:
             return print("Limite de saques excedido")
 
         else:
-            if self.ultima_data_saque != hoje:
-                self.saques = 0
-                self.ultima_data_saque = hoje
+            if self.__ultima_data_saque != hoje:
+                self.__saques = 0
+                self.__ultima_data_saque = hoje
 
-            self.saldo -= valor
-            self.saques += 1
-            self.historico.append(
+            self.__saldo -= valor
+            self.__saques += 1
+            self.__historico.append(
                 {
                     "id": uuid4(),
                     "data": datetime.now().date().isoformat(),
@@ -54,19 +54,20 @@ class SistemaBancario:
                     "operacao": "Saque",
                 }
             )
-            return f"Saque realizado com sucesso! Saldo atual: {self.saldo}"
+            return print(f"Saque realizado com sucesso! Saldo atual: {self.__saldo}")
 
-    def extrato(self):
+    def __extrato(self):
         print("---- Extrato ----")
-        if len(self.historico) == 0:
+        print(f"Cliente: {self.__nome}")
+        if len(self.__historico) == 0:
             print("Não foram realizadas movimentações")
 
-        for transacao in self.historico:
+        for transacao in self.__historico:
             operacao = transacao["operacao"]
             valor = transacao["valor"]
             print(f"{operacao}: R${valor:.2f}")
 
-        print(f"Saldo atual: R${self.saldo:.2f}")
+        print(f"Saldo atual: R${self.__saldo:.2f}")
         print("-----------------")
 
     def operacoes(self):
@@ -82,14 +83,14 @@ class SistemaBancario:
         match operacao_selecionada:
             case "d":
                 valor = int(input("Digite o valor do depósito: "))
-                self.deposito(valor)
+                self.__deposito(valor)
                 self.operacoes()
             case "s":
                 valor = int(input("Digite o valor do saque: "))
-                self.saque(valor)
+                self.__saque(valor)
                 self.operacoes()
             case "e":
-                self.extrato()
+                self.__extrato()
                 self.operacoes()
             case "q":
                 return print("até mais!")
